0e050005da959932d3d9ff1f26b4fe6fdb1ac021
Семенов Семен <simonmen87@gmail.com>
2016-12-04 22:24:15 +0300

    Добавил папки, скрипты инициализации ГУИ

---
+++ Assets/Scripts/GameIni.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameIni : MonoBehaviour {
+
+    MainGUI mainGui = new MainGUI();
+
+	void Start () {
+	     mainGui.Create();
+	}
+
+}
---
+++ Assets/Scripts/MainGUI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class MainGUI : MonoBehaviour
+{
+    private  GameObject mainGUIprefab, canvas;
+
+    private void Initialize()
+    {
+        mainGUIprefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/BattleGUI.prefab");
+        canvas = GameObject.Find("Canvas");
+    }
+
+    public void Create()
+    {
+        Initialize();
+        GameObject sp = Instantiate(mainGUIprefab);
+        sp.transform.SetParent(canvas.transform);
+        sp.GetComponent<RectTransform>().localPosition = new Vector3(0, sp.GetComponent<RectTransform>().localPosition.y);
+    }
+}
