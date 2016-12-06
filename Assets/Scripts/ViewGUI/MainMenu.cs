c51c76817ef3d68e03dd12ebeb191280e290b1ff
Семенов Семен <simonmen87@gmail.com>
2016-12-06 15:35:38 +0300

    Add BtnStart, BtnOptions, BtnExit, MainMenuScript, MainMenuGameObject

---
+++ Assets/Scripts/ViewGUI/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void Play()
+    {
+        GameObject gameIni = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/GameIni.prefab");
+        Instantiate(gameIni);
+        GameObject mainMenu = GameObject.Find("MainMenu");
+        mainMenu.gameObject.SetActive(false);
+        if (mainMenu.gameObject.activeSelf)
+        {
+            mainMenu.gameObject.SetActive(false);
+        }
+
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+}
